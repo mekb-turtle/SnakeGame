58d258f0e8e866be5f323d3c963d340345d9ff40
mekb <mekbgithub@icloud.com>
2023-09-07 16:49:21 +1000

    :tada: Initial commit

---
+++ Snake/SnakeForm.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SnakeForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SnakeForm))
+        Me.box = New System.Windows.Forms.Panel()
+        Me.gameTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.gameOverText = New System.Windows.Forms.Label()
+        Me.winText = New System.Windows.Forms.Label()
+        Me.mainPanel = New System.Windows.Forms.Panel()
+        Me.numericHeight = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.numericWidth = New System.Windows.Forms.NumericUpDown()
+        Me.resumeButton = New System.Windows.Forms.Button()
+        Me.pausedText = New System.Windows.Forms.Label()
+        Me.pauseButton = New System.Windows.Forms.Button()
+        Me.mainPanel.SuspendLayout()
+        CType(Me.numericHeight, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numericWidth, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'box
+        '
+        Me.box.BackColor = System.Drawing.Color.Black
+        Me.box.Location = New System.Drawing.Point(69, 47)
+        Me.box.Margin = New System.Windows.Forms.Padding(2)
+        Me.box.Name = "box"
+        Me.box.Size = New System.Drawing.Size(133, 65)
+        Me.box.TabIndex = 0
+        '
+        'gameTimer
+        '
+        Me.gameTimer.Interval = 300
+        '
+        'startButton
+        '
+        Me.startButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.startButton.ForeColor = System.Drawing.Color.White
+        Me.startButton.Location = New System.Drawing.Point(232, 125)
+        Me.startButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(75, 31)
+        Me.startButton.TabIndex = 1
+        Me.startButton.Text = "Start"
+        Me.startButton.UseVisualStyleBackColor = False
+        '
+        'gameOverText
+        '
+        Me.gameOverText.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.gameOverText.ForeColor = System.Drawing.Color.Red
+        Me.gameOverText.Location = New System.Drawing.Point(232, 97)
+        Me.gameOverText.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.gameOverText.Name = "gameOverText"
+        Me.gameOverText.Size = New System.Drawing.Size(75, 15)
+        Me.gameOverText.TabIndex = 2
+        Me.gameOverText.Text = "Game Over!"
+        Me.gameOverText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.gameOverText.Visible = False
+        '
+        'winText
+        '
+        Me.winText.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.winText.ForeColor = System.Drawing.Color.Gold
+        Me.winText.Location = New System.Drawing.Point(232, 61)
+        Me.winText.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.winText.Name = "winText"
+        Me.winText.Size = New System.Drawing.Size(75, 15)
+        Me.winText.TabIndex = 3
+        Me.winText.Text = "You Win!"
+        Me.winText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.winText.Visible = False
+        '
+        'mainPanel
+        '
+        Me.mainPanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.mainPanel.Controls.Add(Me.numericHeight)
+        Me.mainPanel.Controls.Add(Me.Label2)
+        Me.mainPanel.Controls.Add(Me.Label1)
+        Me.mainPanel.Controls.Add(Me.numericWidth)
+        Me.mainPanel.ForeColor = System.Drawing.Color.Transparent
+        Me.mainPanel.Location = New System.Drawing.Point(209, 170)
+        Me.mainPanel.Margin = New System.Windows.Forms.Padding(2)
+        Me.mainPanel.Name = "mainPanel"
+        Me.mainPanel.Size = New System.Drawing.Size(131, 41)
+        Me.mainPanel.TabIndex = 4
+        '
+        'numericHeight
+        '
+        Me.numericHeight.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.numericHeight.ForeColor = System.Drawing.Color.White
+        Me.numericHeight.Location = New System.Drawing.Point(91, 12)
+        Me.numericHeight.Margin = New System.Windows.Forms.Padding(2)
+        Me.numericHeight.Name = "numericHeight"
+        Me.numericHeight.Size = New System.Drawing.Size(37, 20)
+        Me.numericHeight.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(77, 14)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "x"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(2, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Size:"
+        '
+        'numericWidth
+        '
+        Me.numericWidth.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.numericWidth.ForeColor = System.Drawing.Color.White
+        Me.numericWidth.Location = New System.Drawing.Point(35, 12)
+        Me.numericWidth.Margin = New System.Windows.Forms.Padding(2)
+        Me.numericWidth.Name = "numericWidth"
+        Me.numericWidth.Size = New System.Drawing.Size(37, 20)
+        Me.numericWidth.TabIndex = 2
+        '
+        'resumeButton
+        '
+        Me.resumeButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.resumeButton.ForeColor = System.Drawing.Color.White
+        Me.resumeButton.Location = New System.Drawing.Point(331, 125)
+        Me.resumeButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.resumeButton.Name = "resumeButton"
+        Me.resumeButton.Size = New System.Drawing.Size(75, 31)
+        Me.resumeButton.TabIndex = 5
+        Me.resumeButton.Text = "Resume"
+        Me.resumeButton.UseVisualStyleBackColor = False
+        Me.resumeButton.Visible = False
+        '
+        'pausedText
+        '
+        Me.pausedText.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.pausedText.ForeColor = System.Drawing.Color.White
+        Me.pausedText.Location = New System.Drawing.Point(331, 97)
+        Me.pausedText.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.pausedText.Name = "pausedText"
+        Me.pausedText.Size = New System.Drawing.Size(75, 15)
+        Me.pausedText.TabIndex = 6
+        Me.pausedText.Text = "Paused"
+        Me.pausedText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.pausedText.Visible = False
+        '
+        'pauseButton
+        '
+        Me.pauseButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pauseButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.pauseButton.ForeColor = System.Drawing.Color.White
+        Me.pauseButton.Location = New System.Drawing.Point(436, 8)
+        Me.pauseButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.pauseButton.Name = "pauseButton"
+        Me.pauseButton.Size = New System.Drawing.Size(75, 31)
+        Me.pauseButton.TabIndex = 7
+        Me.pauseButton.Text = "Pause"
+        Me.pauseButton.UseVisualStyleBackColor = False
+        Me.pauseButton.Visible = False
+        '
+        'SnakeForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(519, 256)
+        Me.Controls.Add(Me.pauseButton)
+        Me.Controls.Add(Me.resumeButton)
+        Me.Controls.Add(Me.pausedText)
+        Me.Controls.Add(Me.mainPanel)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.winText)
+        Me.Controls.Add(Me.gameOverText)
+        Me.Controls.Add(Me.box)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MinimumSize = New System.Drawing.Size(270, 170)
+        Me.Name = "SnakeForm"
+        Me.Text = "Snake"
+        Me.mainPanel.ResumeLayout(False)
+        Me.mainPanel.PerformLayout()
+        CType(Me.numericHeight, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numericWidth, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents box As Panel
+    Friend WithEvents gameTimer As Timer
+    Friend WithEvents startButton As Button
+    Friend WithEvents gameOverText As Label
+    Friend WithEvents winText As Label
+    Friend WithEvents mainPanel As Panel
+    Friend WithEvents numericWidth As NumericUpDown
+    Friend WithEvents numericHeight As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents resumeButton As Button
+    Friend WithEvents pausedText As Label
+    Friend WithEvents pauseButton As Button
+End Class
---
+++ Snake/SnakeForm.vb
@@ -0,0 +1,383 @@
+﻿Imports System.Runtime.CompilerServices.RuntimeHelpers
+Imports Snake.SnakeGameUtils
+
+Public Class SnakeForm
+    Dim Game As New SnakeGame(16, 16)
+
+    Private Sub SnakeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        numericWidth.Minimum = 4
+        numericHeight.Minimum = 4
+        numericWidth.Maximum = 32
+        numericHeight.Maximum = 32
+        numericWidth.Value = 16
+        numericHeight.Value = 16
+        UpdateSize()
+        SetMenuVisible(True)
+    End Sub
+
+    Private Const CellMargin As Single = 0.1
+
+    Private snakeColor As New SolidBrush(Color.LimeGreen)
+    Private snakeDeadColor As New SolidBrush(Color.Red)
+    Private appleColor As New SolidBrush(Color.Red)
+
+    Private Function AddNeighborCellRectangle(cell As Position, otherCell As Position, rect As Rectangle, rects As List(Of Rectangle)) As List(Of Rectangle)
+        Dim marg As Single = CellMargin * renderScale
+
+        If otherCell + Position.FromDirection(Direction.Right) = cell Then
+            rects.Add(New Rectangle(
+                rect.X - marg, rect.Y,
+                renderScale * 0.5, rect.Height + 1
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Down) = cell Then
+            rects.Add(New Rectangle(
+                rect.X, rect.Y - marg,
+                rect.Width + 1, renderScale * 0.5
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Left) = cell Then
+            rects.Add(New Rectangle(
+                rect.X - marg + renderScale * 0.5, rect.Y,
+                renderScale * 0.5 + 1, rect.Height + 1
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Up) = cell Then
+            rects.Add(New Rectangle(
+                rect.X, rect.Y - marg + renderScale * 0.5,
+                rect.Width + 1, renderScale * 0.5 + 1
+            ))
+        End If
+
+        Return rects
+    End Function
+
+    Private Function AddNeighborEndCellRectangle(cell As Position, otherCell As Position, rect As Rectangle, rects As List(Of Rectangle)) As List(Of Rectangle)
+        If otherCell + Position.FromDirection(Direction.Right) = cell Then
+            rects.Add(New Rectangle(
+                rect.X, rect.Y,
+                rect.Width * 0.5, rect.Height
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Down) = cell Then
+            rects.Add(New Rectangle(
+                rect.X, rect.Y,
+                rect.Width, rect.Height * 0.5
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Left) = cell Then
+            rects.Add(New Rectangle(
+                rect.Width * 0.5 + rect.X, rect.Y,
+                rect.Width * 0.5, rect.Height
+            ))
+        ElseIf otherCell + Position.FromDirection(Direction.Up) = cell Then
+            rects.Add(New Rectangle(
+                rect.X, rect.Height * 0.5 + rect.Y,
+                rect.Width, rect.Height * 0.5
+            ))
+        End If
+
+        Return rects
+    End Function
+
+    Private renderScale As Single
+
+    Private Sub box_Paint(sender As Object, e As PaintEventArgs) Handles box.Paint
+        Dim marg As Single = CellMargin * renderScale
+
+        Using g As Graphics = box.CreateGraphics()
+            g.Clear(Color.Transparent)
+
+            ' border
+            g.DrawRectangle(
+                New Pen(Color.Red),
+                New Rectangle(0, 0, box.Width - 1, box.Height - 1)
+            )
+
+            Dim appleRect As New Rectangle(
+                Math.Floor(Game.ApplePosition.X * renderScale + marg),
+                Math.Floor(Game.ApplePosition.Y * renderScale + marg),
+                Math.Ceiling((1 - (CellMargin * 2)) * renderScale),
+                Math.Ceiling((1 - (CellMargin * 2)) * renderScale)
+            )
+            g.FillEllipse(appleColor, appleRect)
+
+            For i = 0 To Game.SnakeCells.Count - 1
+                Dim cell As Position = Game.SnakeCells(i)
+                Dim initialRect As New Rectangle(
+                    marg + cell.X * renderScale,
+                    marg + cell.Y * renderScale,
+                    renderScale - (CellMargin * 2) * renderScale,
+                    renderScale - (CellMargin * 2) * renderScale
+                )
+                Dim rects As New List(Of Rectangle)
+
+                If i > 0 Then
+                    rects = AddNeighborCellRectangle(cell, Game.SnakeCells(i - 1), initialRect, rects)
+                ElseIf Game.SnakeCells.Count > 1 Then
+                    rects = AddNeighborEndCellRectangle(cell, Game.SnakeCells(1), initialRect, rects)
+                End If
+
+                If i < Game.SnakeCells.Count - 1 Then
+                    rects = AddNeighborCellRectangle(cell, Game.SnakeCells(i + 1), initialRect, rects)
+                ElseIf Game.SnakeCells.Count > 1 Then
+                    rects = AddNeighborEndCellRectangle(cell, Game.SnakeCells(i - 1), initialRect, rects)
+                End If
+
+                For Each rect In rects
+                    g.FillRectangle(snakeColor, rect)
+                Next
+
+                g.FillEllipse(snakeColor, initialRect)
+
+                If i = Game.SnakeCells.Count - 1 And Not Game.SnakeAlive And Not Game.SnakeWin Then
+                    g.FillEllipse(snakeDeadColor, initialRect)
+                End If
+            Next
+        End Using
+    End Sub
+
+    Private Sub UpdateButtons()
+        Dim winWidth As Single = ClientSize.Width
+        Dim winHeight As Single = ClientSize.Height
+
+        startButton.Left = (winWidth - startButton.Width) * 0.5
+        startButton.Top = (winHeight - startButton.Height) * 0.5
+
+        resumeButton.Left = (winWidth - resumeButton.Width) * 0.5
+        resumeButton.Top = (winHeight - resumeButton.Height) * 0.5
+
+        gameOverText.Left = (winWidth - gameOverText.Width) * 0.5
+        gameOverText.Top = startButton.Top - (Height * 0.1) - 20
+
+        pausedText.Left = (winWidth - pausedText.Width) * 0.5
+        pausedText.Top = gameOverText.Top
+
+        winText.Left = (winWidth - winText.Width) * 0.5
+        winText.Top = gameOverText.Top
+
+        mainPanel.Left = (winWidth - mainPanel.Width) * 0.5
+        mainPanel.Top = startButton.Top + startButton.Height + (Height * 0.05)
+
+        If Paused Then
+            startButton.Top += resumeButton.Height + (Height * 0.05)
+        End If
+    End Sub
+
+    Private Sub UpdateBox()
+        UpdateButtons()
+
+        Dim winWidth As Single = ClientSize.Width
+        Dim winHeight As Single = ClientSize.Height
+
+        Dim gameWidth As Single = Game.GameSize.X
+        Dim gameHeight As Single = Game.GameSize.Y
+
+        If gameWidth / gameHeight > winWidth / winHeight Then
+            renderScale = winWidth / gameWidth
+            box.Left = 0
+            box.Top = winHeight * 0.5 - (gameHeight * renderScale * 0.5)
+            box.Width = winWidth
+            box.Height = renderScale * gameHeight
+        Else
+            renderScale = winHeight / gameHeight
+            box.Left = winWidth * 0.5 - (gameWidth * renderScale * 0.5)
+            box.Top = 0
+            box.Width = renderScale * gameWidth
+            box.Height = winHeight
+        End If
+
+        box.Invalidate()
+    End Sub
+
+    Private Sub SnakeForm_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        UpdateBox()
+    End Sub
+
+    Private Sub gameTimer_Tick(sender As Object, e As EventArgs) Handles gameTimer.Tick
+        If Game.SnakeAlive Then
+            If changeDirectionQueue.Count > 0 Then
+                Game.SnakeDirection = changeDirectionQueue(0)
+                changeDirectionQueue.RemoveAt(0)
+            End If
+        End If
+
+        Game.Advance()
+        box.Invalidate()
+
+        If Not Game.SnakeAlive Then
+            StopGame()
+        End If
+    End Sub
+
+    Private changeDirectionQueue As New List(Of Direction)
+
+    Private Paused As Boolean = False
+
+    Private Sub TogglePause()
+        If Not Game.SnakeAlive Or Not firstStarted Then
+            Paused = False
+            StartGame()
+            SetMenuVisible(False)
+            Return
+        End If
+
+        If Paused Then
+            changeDirectionQueue.Clear()
+            Paused = False
+            gameTimer.Start()
+            SetMenuVisible(False)
+            pauseButton.Focus()
+        Else
+            changeDirectionQueue.Clear()
+            Paused = True
+            gameTimer.Stop()
+            SetMenuVisible(False)
+            resumeButton.Focus()
+        End If
+    End Sub
+
+    Private Sub SnakeForm_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles MyBase.PreviewKeyDown
+        If HandleKeyDown(e.KeyCode) Then
+            e.IsInputKey = False
+        End If
+    End Sub
+
+    Private Function HandleKeyDown(keyCode As Keys) As Boolean
+        Dim dir As Direction = Direction.None
+        Select Case keyCode
+            Case 27, 13, 80
+                GoTo KeyPause
+            Case 65
+                dir = Direction.Left
+            Case 87
+                dir = Direction.Up
+            Case 68
+                dir = Direction.Right
+            Case 83
+                dir = Direction.Down
+                Exit Select
+            Case Else
+                Return False
+        End Select
+
+        If False Then
+KeyPause:
+            TogglePause()
+            Return True
+        End If
+
+        Return HandleMove(dir)
+    End Function
+
+    Private Function HandleMove(dir As Direction) As Boolean
+        If dir = Direction.None Then
+            Return False
+        End If
+
+        If Paused Or Not Game.SnakeAlive Or Not firstStarted Then
+            TogglePause()
+        End If
+
+        If changeDirectionQueue.Count < 3 Then
+            If Game.SnakeLength > 1 Then
+                If changeDirectionQueue.Count = 0 Then
+                    If IsOppositeDirection(dir, Game.SnakeDirection) Then
+                        GoTo skipAddQueue
+                    End If
+                Else
+                    If IsOppositeDirection(dir, changeDirectionQueue.Last()) Then
+                        GoTo skipAddQueue
+                    End If
+                End If
+            End If
+
+            changeDirectionQueue.Add(dir)
+skipAddQueue:
+        End If
+        Return True
+    End Function
+
+    Private firstStarted As Boolean = False
+
+    Private Sub SetMenuVisible(visible As Boolean)
+        resumeButton.Visible = Paused
+        resumeButton.Enabled = Paused
+        pausedText.Visible = Paused
+        startButton.Visible = Paused Or visible
+        startButton.Enabled = startButton.Visible
+        gameOverText.Visible = visible And Not Game.SnakeWin And firstStarted
+        winText.Visible = visible And Game.SnakeWin And firstStarted
+        mainPanel.Visible = visible
+        numericWidth.Enabled = visible
+        numericHeight.Enabled = visible
+        pauseButton.Visible = Not visible And Not Paused
+        pauseButton.Enabled = pauseButton.Visible
+        If visible Then
+            startButton.Focus()
+        End If
+        UpdateButtons()
+    End Sub
+
+    Private Sub StopGame()
+        changeDirectionQueue.Clear()
+        gameTimer.Stop()
+        SetMenuVisible(True)
+        Paused = False
+    End Sub
+
+    Private Sub UpdateSize()
+        Game.SetSize(numericWidth.Value, numericHeight.Value)
+        UpdateBox()
+    End Sub
+
+    Private Sub StartGame()
+        changeDirectionQueue.Clear()
+
+        Paused = False
+
+        UpdateBox()
+        If firstStarted Then
+            Game.Restart()
+            Game.MoveApple()
+        End If
+
+        firstStarted = True
+
+        gameTimer.Start()
+
+        SetMenuVisible(False)
+
+        box.Invalidate()
+
+        startButton.Text = "Restart"
+
+        pauseButton.Focus()
+    End Sub
+
+    Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        StartGame()
+    End Sub
+
+    Private Sub numericWidth_ValueChanged(sender As Object, e As EventArgs) Handles numericWidth.ValueChanged, numericHeight.ValueChanged
+        UpdateSize()
+    End Sub
+
+    Private Sub resumeButton_Click(sender As Object, e As EventArgs) Handles resumeButton.Click
+        TogglePause()
+    End Sub
+
+    Private Sub pauseButton_Click(sender As Object, e As EventArgs) Handles pauseButton.Click
+        TogglePause()
+    End Sub
+
+    Private Sub resumeButton_KeyDown(sender As Object, e As KeyEventArgs) Handles resumeButton.KeyDown
+        HandleKeyDown(e.KeyCode)
+        e.SuppressKeyPress = True
+    End Sub
+
+    Private Sub startButton_KeyDown(sender As Object, e As KeyEventArgs) Handles startButton.KeyDown
+        HandleKeyDown(e.KeyCode)
+        e.SuppressKeyPress = True
+    End Sub
+
+    Private Sub pauseButton_KeyDown(sender As Object, e As KeyEventArgs) Handles pauseButton.KeyDown
+        HandleKeyDown(e.KeyCode)
+        e.SuppressKeyPress = True
+    End Sub
+End Class
