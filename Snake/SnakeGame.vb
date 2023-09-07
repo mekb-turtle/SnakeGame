f7e6e2350c6fa84f117ce40766c2bf0c5354951d
mekb <mekbgithub@icloud.com>
2023-09-07 16:49:21 +1000

    :tada: Initial commit

---
+++ Snake/SnakeGame.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Numerics
+Imports Snake.SnakeGameUtils
+
+Public Class SnakeGame
+    Public SnakeCells As New List(Of Position)
+    Public SnakeDirection As Direction = Direction.None
+    Public SnakeLength As Integer = 1
+    Public SnakePosition As New Position
+    Public GameSize As Position
+    Public SnakeAlive As Boolean = True
+    Public SnakeWin As Boolean = False
+    Public ApplePosition As Position
+
+    Private rng As New Random(Guid.NewGuid().GetHashCode())
+
+    Public Sub New(X As Integer, Y As Integer)
+        GameSize = New Position(X, Y)
+        Restart()
+        MoveApple()
+    End Sub
+
+    Public Sub SetSize(X As Integer, Y As Integer)
+        GameSize.X = X
+        GameSize.Y = Y
+        Restart()
+        MoveApple()
+    End Sub
+
+    Private Sub CenterSnakePosition()
+        SnakePosition.X = (GameSize.X - 1) / 2
+        SnakePosition.Y = (GameSize.Y - 1) / 2
+    End Sub
+
+    Public Sub Restart()
+        SnakeDirection = Direction.None
+        CenterSnakePosition()
+        SnakeLength = 1
+        SnakeAlive = True
+        SnakeWin = False
+        SnakeCells.Clear()
+        SnakeCells.Add(SnakePosition)
+    End Sub
+
+    Public Function MoveApple() As Boolean
+        Dim Cells As New List(Of Position)
+        For x = 0 To GameSize.X - 1
+            For y = 0 To GameSize.Y - 1
+                For Each cell In SnakeCells
+                    If cell.X = x And cell.Y = y Then
+                        GoTo skipAddRandomPosition
+                    End If
+                Next
+                Cells.Add(New Position(x, y))
+skipAddRandomPosition:
+            Next
+        Next
+
+        If Cells.Count = 0 Then
+            Return False
+        End If
+
+        ApplePosition = Cells(rng.Next(Cells.Count))
+        Return True
+    End Function
+
+    Private Function OutOfBounds(pos As Position) As Boolean
+        Return pos.X < 0 Or pos.Y < 0 Or pos.X >= GameSize.X Or pos.Y >= GameSize.Y
+    End Function
+
+    Public Sub Advance()
+        If Not SnakeAlive Then
+            Return
+        End If
+
+        If SnakeDirection = Direction.None Then
+            Return
+        End If
+
+        SnakePosition += Position.FromDirection(SnakeDirection)
+
+        If OutOfBounds(SnakePosition) Then
+            SnakeAlive = False
+            Return
+        End If
+
+        Dim crash As Boolean = False
+        Dim apple As Boolean = False
+        For i = 1 To SnakeCells.Count - 1
+            If SnakeCells(i) = SnakePosition Then
+                crash = True
+            End If
+        Next
+
+        SnakeCells.Add(SnakePosition)
+
+        If SnakePosition = ApplePosition And Not crash Then
+            SnakeLength += 1
+            apple = True
+        End If
+
+        While SnakeCells.Count > SnakeLength
+            SnakeCells.RemoveAt(0)
+        End While
+
+        If apple Then
+            If Not MoveApple() Then
+                SnakeAlive = False
+                SnakeWin = True
+            End If
+        End If
+
+        If crash Then
+            SnakeAlive = False
+        End If
+    End Sub
+End Class
---
+++ Snake/SnakeGameUtils.vb
@@ -0,0 +1,83 @@
+﻿Public Class SnakeGameUtils
+    Public Enum Direction
+        None
+        Left
+        Up
+        Right
+        Down
+    End Enum
+
+    Public Shared Function IsOppositeDirection(a As Direction, b As Direction) As Boolean
+        If a = Direction.Left And b = Direction.Right Then
+            Return True
+        ElseIf a = Direction.Up And b = Direction.Down Then
+            Return True
+        ElseIf a = Direction.Right And b = Direction.Left Then
+            Return True
+        ElseIf a = Direction.Down And b = Direction.Up Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Class Position
+        Public X As Integer = 0
+        Public Y As Integer = 0
+
+        Public Sub New(X As Integer, Y As Integer)
+            Me.X = X
+            Me.Y = Y
+        End Sub
+
+        Public Sub New()
+
+        End Sub
+
+        Public Shared Function FromDirection(Dir As Direction)
+            Select Case Dir
+                Case Direction.Up
+                    Return New Position(0, -1)
+                Case Direction.Right
+                    Return New Position(1, 0)
+                Case Direction.Down
+                    Return New Position(0, 1)
+                Case Direction.Left
+                    Return New Position(-1, 0)
+            End Select
+            Return New Position(0, 0)
+        End Function
+
+        Public Shared Operator +(A As Position, B As Position)
+            Return New Position(A.X + B.X, A.Y + B.Y)
+        End Operator
+        Public Shared Operator -(A As Position, B As Position)
+            Return New Position(A.X - B.X, A.Y - B.Y)
+        End Operator
+        Public Shared Operator *(A As Position, B As Position)
+            Return New Position(A.X * B.X, A.Y * B.Y)
+        End Operator
+        Public Shared Operator /(A As Position, B As Position)
+            Return New Position(A.X / B.X, A.Y / B.Y)
+        End Operator
+
+        Public Shared Operator +(A As Position, B As Integer)
+            Return New Position(A.X + B, A.Y + B)
+        End Operator
+        Public Shared Operator -(A As Position, B As Integer)
+            Return New Position(A.X - B, A.Y - B)
+        End Operator
+        Public Shared Operator *(A As Position, B As Integer)
+            Return New Position(A.X * B, A.Y * B)
+        End Operator
+        Public Shared Operator /(A As Position, B As Integer)
+            Return New Position(A.X / B, A.Y / B)
+        End Operator
+
+        Public Shared Operator =(A As Position, B As Position)
+            Return A.X = B.X And A.Y = B.Y
+        End Operator
+        Public Shared Operator <>(A As Position, B As Position)
+            Return A.X <> B.X Or A.Y <> B.Y
+        End Operator
+    End Class
+End Class
